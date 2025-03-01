d8cb1a80b9520c9f734c282c879af773fc3f2e64
Dingobad <kirillmelnikov008@gmail.com>
2025-03-02 02:13:50 +0700

    Add files via upload

---
+++ BlockDemo.java
@@ -0,0 +1,13 @@
+class BlockDemo
+{
+	public static void main(String[] args) {
+		   double i, j, d;
+		   i = 5;
+		   j = 10;
+		   if(i != 0){
+		       System.out.println("Значение i Не равно нулю.");
+		       d = j/i;
+		       System.out.println("Результат j / i = " + d);
+		   }
+	}
+}
